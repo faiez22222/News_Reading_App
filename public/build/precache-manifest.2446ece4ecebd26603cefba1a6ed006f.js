74e0d1e7def532800df3574c50ada8f0ef489e16
Mohd Faiez <faiezmohd43@gmail.com>
2022-08-01 14:47:22 +0530

    final commit
    final commit

---
+++ public/build/precache-manifest.2446ece4ecebd26603cefba1a6ed006f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0042e9f5bfc9beca991a7164567ad47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4569d54117a8bf7a9a2",
+    "url": "/static/css/main.e23e6b48.chunk.css"
+  },
+  {
+    "revision": "a014259eabce13d9ca54",
+    "url": "/static/js/2.9dca3890.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9dca3890.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4569d54117a8bf7a9a2",
+    "url": "/static/js/main.dce35282.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "62e0aad0b912528563e1b22564370734",
+    "url": "/static/media/logo.62e0aad0.png"
+  }
+]);
